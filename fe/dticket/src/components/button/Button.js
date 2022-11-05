3de4474bf3ea30a592822cc620100563f872a7e1
unknown <marctan2705@gmail.com>
2022-11-04 23:18:18 -0700

    change on hover button meme

--- fe/dticket/src/components/button/Button.js
+++ fe/dticket/src/components/button/Button.js
@@ -10,12 +10,12 @@ function Button(props) {
     // }
     // styles = styles + text + bg
 
-    const reverse = "opacity-95 px-4 py-2 rounded-md font-bold hover:opacity-100 text-rose-400 bg-white"
+    const reverse = "opacity-95 px-4 py-2 rounded-md font-bold hover:opacity-100 text-rose-400 bg-white ease-in-out duration-100"
 
-    const noReverse = "opacity-95 px-4 py-2 rounded-md font-bold hover:opacity-100 text-white bg-rose-400"
+    const noReverse = "opacity-95 px-4 py-2 rounded-md font-bold hover:opacity-100 text-white bg-rose-400 ease-in-out duration-100"
     // console.log(styles)
     return ( 
-        <button className={`${props.x} ${props.reverse ? reverse:noReverse}`}>
+        <button onMouseOver={props.onHover} className={`${props.x} ${props.reverse ? reverse:noReverse}`}>
             <div>{props.text}</div>
         </button>
      );
