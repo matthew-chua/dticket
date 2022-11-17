56a4c0ba78955eaf1fd03333125d02f9d3b0ab13
Keane Kwa <hello@keanekwa.com>
2022-11-17 10:44:19 -0800

    cleanup verifypage

--- fe/dticket/src/pages/VerifyPage.js
+++ fe/dticket/src/pages/VerifyPage.js
@@ -1,28 +1,21 @@
-import React, { useState, useContext } from "react"
+import React, { useState } from "react"
 import Button from "../components/button/Button"
 import Modal from "../components/modal/Modal"
 import tick from "../assets/tick.png"
 import cross from "../assets/cross.png"
-import UserContext from "../Contexts/usercontext"
 import { ethers } from "ethers"
 import MotherABI from "../ABIs/mother"
 
 export default function VerifyPage() {
 	const [validity, setValidity] = useState(null)
 	const [contract, setContract] = useState(null)
-	const [tokenID, setTokenID] = useState(null)
-	const user = useContext(UserContext)
 	const CONTRACT = "0xe63032C48f9e66A63149e66401091E69d2060ffB"
-	// console.log(contract)
 
 	const submitHandler = async (e) => {
 		e.preventDefault()
 		let provider = ethers.getDefaultProvider("https://goerli.optimism.io")
 		const new_contract = new ethers.Contract(CONTRACT, MotherABI, provider)
-		// console.log(new_contract)
-		// console.log(contract)
 		let tx = await new_contract.verify(contract)
-		// await tx.wait();
 		setValidity(tx)
 	}
 	return (
@@ -48,16 +41,7 @@ export default function VerifyPage() {
 						onChange={(e) => {
 							setContract(e.target.value)
 						}}></input>
-					<div className="text-3xl mt-10 w-4/5 text-left">TokenID</div>
-					<input
-						className="text-black mt-4 h-12 w-4/5 rounded-lg"
-						type="text"
-						onChange={(e) => {
-							setTokenID(e.target.value)
-						}}></input>
-					{/* <div className='flex flex row justify-end w-4/5 '> */}
 					<Button x="mt-10 w-32" text="Verify Ticket" onClick={submitHandler} />
-					{/* </div> */}
 				</form>
 			</div>
 		</>
